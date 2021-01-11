925806b252907871841c2be6f45586c9da2e6d8b
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-01-11 15:16:12 +0000

    top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,9 +3,19 @@
 require 'oystercard'
 
 describe Oystercard do
-  describe 'balance' do
+  describe '#balance' do
     it 'expects initial balance to be 0' do
       expect(subject.balance).to eq 0
     end
+  end
+
+  describe '#top_up' do
+    it 'responds to .top_up' do
+      expect(subject).to respond_to :top_up
+    end
+    it 'tops up balance by 5' do
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
+    end 
   end
 end
